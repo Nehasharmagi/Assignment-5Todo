54276865ef7064938f402e0f00d770bdd3f07646
Neha Sharma <NS232@myscc.ca>
2021-11-10 23:00:27 -0500

    Initial commit

---
+++ BlazorApp/Character.cs
@@ -0,0 +1,11 @@
+public class Character
+{
+
+private string newTodo;
+
+    public string name{get;set;}
+    public int age{get;set;}
+    public string profession{get;set;}
+
+    public bool isficational{get;set;}
+}
